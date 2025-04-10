3c70fa2406c6f52473934f5a922aa864b0bf3e5c
Mirera <phudongsi1@gmail.com>
2025-04-10 12:01:58 +0700

    config temp CORS

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 import uvicorn
 from database import engine, Base
 from routers import userRouter, projectRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+#add middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to match the frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
