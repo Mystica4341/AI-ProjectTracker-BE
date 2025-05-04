71a123efbf4ef0c8d9eb111493797803bb1b9701
Mirera <phudongsi1@gmail.com>
2025-05-04 16:37:33 +0700

    refactor database.py

--- database.py
+++ database.py
@@ -2,17 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from qdrant_client import QdrantClient
+from dotenv import load_dotenv
+import os
 
-# Replace with your actual database connection details
-DATABASE_URL = "mssql+pyodbc://sa:12345@localhost/TrackingWebsite?driver=ODBC+Driver+17+for+SQL+Server"
+#read .env file
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+QDRANT_HOST = os.getenv("QDRANT_HOST")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 qdrant_client = QdrantClient(
-    host = '49e3e764-01cb-441e-8910-b4bcc220aa17.us-east-1-0.aws.cloud.qdrant.io',
-    api_key= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.CN3ZUe9y5skPuYA_wc5b_j1-5yfv4BPEnsw62UmeI-k',
+    host = QDRANT_HOST,
+    api_key= QDRANT_API_KEY,
     port= 6333
 )
 # Test the connection
